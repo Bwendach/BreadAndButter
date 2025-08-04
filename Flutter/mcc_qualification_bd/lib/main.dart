0aee5fd0136ca180b8ae1afaa5db49b14e212e58
Bwendach <82569226+Bwendach@users.noreply.github.com>
2025-08-04 09:33:04 +0700

    remove env

--- Flutter/mcc_qualification_bd/lib/main.dart
+++ Flutter/mcc_qualification_bd/lib/main.dart
@@ -4,11 +4,7 @@ import 'package:bread_and_butter/screens/login_screen.dart';
 import 'package:bread_and_butter/utils/colors.dart';
 import 'package:bread_and_butter/utils/theme_provider.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
